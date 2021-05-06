1909eeaff5a397df2c8bf27bc398bb4f0b1db03e
zhoulin <34066836+boorZ@users.noreply.github.com>
2021-05-06 10:11:31 +0800

    init

---
+++ spring-boot-admin/spring-boot-admin-service/src/main/java/com/example/springbootadminservice/SpringBootAdminServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootadminservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminServiceApplication.class, args);
+    }
+
+}
