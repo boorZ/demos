c547cc7dcbcac7e48d6b162b430a7f9768328459
周林 <zhoulin5>
2021-07-01 21:54:12 +0800

    init

--- noask-qa-es/src/main/java/com/example/noaskqaes/AnalysisQAService.java
+++ noask-qa-es/src/main/java/com/example/noaskqaes/AnalysisQAService.java
@@ -1,9 +1,10 @@
 package com.example.noaskqaes;
 
+import com.example.noaskqaes.vo.dto.QaDTO;
 import org.junit.Test;
 
-import java.util.Collection;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * 分析问答
@@ -13,11 +14,44 @@ import java.util.Map;
  * @date 2021/7/1 17:43
  */
 public class AnalysisQAService {
+    protected List<QaDTO> qaData;
+    protected List<String> szs;
+
+    {
+        szs = Arrays.asList(("企业所得税,个人所得税,增值税,消费税,车辆购置税,房产税,城镇土地使用税,土地增值税,耕地占用税,资源税,契税,印花税,车船税,烟叶税,环境保护税," +
+                "城市维护建设税,船舶吨税,关税,营业税,社会保险费,工会经费,教育费附加,地方教育费附加,非税收入").split(","));
+        qaData = new ArrayList<>();
+        Map<String, String> map = IFileService.readQa();
+        map.forEach((k, v) -> qaData.add(new QaDTO(k, v)));
+    }
+    @Test
+    public void sz() {
+        String str = "非直系亲属继承是否缴纳土地增值税？";
+        for (String sz : szs) {
+            if (str.contains(sz)) {
+                System.out.println(sz);
+            }
+        }
+    }
     @Test
     public void tests() {
-        Map<String, String> map = IFileService.readQa();
-        Collection<String> values = map.values();
-//        values.stream().filter(f -> f.contains("个人"))
+        List<QaDTO> filters = new ArrayList<>();
+        for (String sz : szs) {
+            List<QaDTO> filter = containName(sz);
+            System.out.println(sz + "\t" + filter.size());
+            filters.addAll(filter);
+        }
+        Map<String, List<QaDTO>> filtersGroup = filters.stream().collect(Collectors.groupingBy(QaDTO::getDocId));
+        List<QaDTO> repetition = filtersGroup.values().stream().filter(f -> f.size() > 1).map(m -> m.get(0)).collect(Collectors.toList());
+
+
+        List<QaDTO> qaDataExtends = new ArrayList<>(qaData);
+        qaDataExtends.removeAll(filters);
+        List<QaDTO> 个税 = qaDataExtends.stream().filter(f -> f.getDocName().contains("个税")).collect(Collectors.toList());
         System.out.println();
+    }
+
+    public List<QaDTO> containName(String str) {
+        return qaData.stream().filter(f -> f.getDocName().contains(str)).collect(Collectors.toList());
     }
 }
